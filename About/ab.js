31fe88df47284af65a1645e2a7a8fcf316305530
animartirosyan2006 <124518025+animartirosyan2006@users.noreply.github.com>
2023-02-05 21:33:55 +0400

    Add files via upload

---
+++ About/ab.js
@@ -0,0 +1,9 @@
+// window.onscroll = function das() {
+//     console.log(window.scrollY)
+//     if (window.scrollY > 500) {
+//       document.getElementById("o1").className = "nor";
+//     }
+//     else {
+//       document.getElementById("o1").className = "apee";
+
+//     }}
---
+++ test/test.js
@@ -0,0 +1,24 @@
+function mek(){
+    document.getElementById("b1").style.background="orange"
+    document.getElementById("b2").style.background="#f1f1f1"
+    document.getElementById("b3").style.background="#f1f1f1"
+    document.getElementById("i1").style="opacity:1;z-index:3"
+    document.getElementById("i2").style="opacity:0;z-index:1"
+    document.getElementById("i3").style="opacity:0;z-index:2"
+}
+function erku(){
+    document.getElementById("b2").style.background="orange"
+    document.getElementById("b3").style.background="#f1f1f1"
+    document.getElementById("b1").style.background="#f1f1f1"
+    document.getElementById("i2").style="opacity:1;z-index:3"
+    document.getElementById("i1").style="opacity:0;z-index:1"
+    document.getElementById("i3").style="opacity:0;z-index:2"
+}
+function ereq(){
+    document.getElementById("b3").style.background="orange"
+    document.getElementById("b2").style.background="#f1f1f1"
+    document.getElementById("b1").style.background="#f1f1f1"
+    document.getElementById("i3").style="opacity:1;z-index:3"
+    document.getElementById("i2").style="opacity:0;z-index:1"
+    document.getElementById("i1").style="opacity:0;z-index:2"
+}
